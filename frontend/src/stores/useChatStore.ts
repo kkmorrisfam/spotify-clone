21d5e95340cdd76ff7866f149973d2c14023b8d5
Kerri Morris <kkmorrisfam@hotmail.com>
2025-10-23 20:44:32 -0700

    start freinds section

---
+++ frontend/src/stores/useChatStore.ts
@@ -0,0 +1,30 @@
+import { axiosInstance } from "@/lib/axios";
+import { create } from "zustand";
+
+interface ChatStore {
+    users: any[];
+    fetchUsers: () => Promise<void>;
+    isLoading: boolean;
+    error: string | null;
+
+}
+
+export const useChatStore = create<ChatStore>((set) => ({
+    users: [],
+    isLoading: false,
+    error: null,
+    
+    fetchUsers : async () => {
+        set({ isLoading: true, error: null});
+        try {
+            const response = await axiosInstance.get("/users");
+            console.log("Fetched users from backend:", response.data);
+            set ({ users: response.data});
+        } catch (error:any) {
+            console.error("Error fetching users:", error);
+            set({error: error.response.data.message});
+        } finally {
+            set({ isLoading: false });
+        }
+    }
+}))
